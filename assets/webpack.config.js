943e48360e9d2f4ced2eaea785f66700d5bcf416
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-23 18:37:00 +0900

    using vue components

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,6 +4,13 @@ module.exports = {
       filename: 'index.js',
       path: `${__dirname}/` 
     },
+    module: {
+        rules: [
+        {
+            test: /\.vue$/,
+            use: 'vue-loader'
+        }]
+    },
     resolve: {
         alias: {
           'vue$': 'vue/dist/vue.esm.js',
