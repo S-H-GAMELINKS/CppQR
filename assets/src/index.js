a265c7a798fde321f3d1f80405ba48486117afa9
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-23 18:41:01 +0900

    using bootstrap-umi

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,5 +1,10 @@
 import Vue from "vue";
 
+import * as BootstrapUmi from "bootstrap-umi";
+import "bootstrap-umi/dist/css/bootstrap.css";
+
+Vue.use(BootstrapUmi);
+
 const app = new Vue({
     el: "#app",
 })
