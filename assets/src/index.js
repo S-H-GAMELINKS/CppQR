0162fe8345f90e655e31f16a77d162c9db4bed36
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-23 18:48:42 +0900

    using QR.vue

--- assets/src/index.js
+++ assets/src/index.js
@@ -4,12 +4,14 @@ import * as BootstrapUmi from "bootstrap-umi";
 import "bootstrap-umi/dist/css/bootstrap.css";
 
 import Header from "../components/Header.vue";
+import QR from "../components/QR.vue";
 
 Vue.use(BootstrapUmi);
 
 const app = new Vue({
     el: "#app",
     components: {
-        'nav-bar': Header
+        'nav-bar': Header,
+        'qr-load': QR
     }
 })
