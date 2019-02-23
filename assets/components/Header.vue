16633247097e3342f8187d9cf99cd5ccf4c79d7b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-23 18:42:17 +0900

    add Header.vue

---
+++ assets/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="/">C++ QR Loader</a>
+        </nav>
+    </div>    
+</template>
