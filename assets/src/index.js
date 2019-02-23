62ed81a6cf5433e77e2674d3af67088b0de67627
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-23 18:43:12 +0900

    using Header components

--- assets/src/index.js
+++ assets/src/index.js
@@ -3,8 +3,13 @@ import Vue from "vue";
 import * as BootstrapUmi from "bootstrap-umi";
 import "bootstrap-umi/dist/css/bootstrap.css";
 
+import Header from "../components/Header.vue";
+
 Vue.use(BootstrapUmi);
 
 const app = new Vue({
     el: "#app",
+    components: {
+        'nav-bar': Header
+    }
 })
